28044cfc9f23899201a06f83d26c96a60857db08
Assem Abdirashova <asemsuper2001@mail.ru>
2020-10-28 12:10:03 +0300

    change

--- startup/settings.py
+++ startup/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
 
+
     'off-time.herokuapp.com/',
     '127.0.0.1'
 ]
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
